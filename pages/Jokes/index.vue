d843b1a51fb95f1a464987112a63e067332c33c1
Seybel <seyibello31@gmail.com>
2020-07-17 16:12:53 +0100

    initial commit

---
+++ pages/Jokes/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>jokes</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+data() {
+    return {
+        jokes: []
+    }
+},
+created () {
+    this.getDadJokes()
+},
+methods: {
+    async getDadJokes() {
+        const config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+
+        try {
+           const { res } = await axios.get('https://icanhazdadjoke.com/search', config)
+           console.log(res)
+            // this.jokes = res
+        } catch (error) {
+            console.error(error)
+        }
+    }
+},
+head() {
+    return {
+        title: 'Dad Jokes',
+        meta: [
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'Best place for corny dad jokes'
+            }
+        ]
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
